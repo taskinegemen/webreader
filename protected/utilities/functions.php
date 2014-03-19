e87c54c1e435d4258bc826c966a492ade965a453
reset <reset>
2014-03-19 14:10:54 +0000

    Header Evennt Added

--- protected/utilities/functions.php
+++ protected/utilities/functions.php
@@ -6,6 +6,41 @@
 */
 class functions
 {
+
+     /**
+     * Attach (or remove) multiple callbacks to an event and trigger those callbacks when that event is called.
+     *
+     * @param string $event name
+     * @param mixed $value the optional value to pass to each callback
+     * @param mixed $callback the method or function to call - FALSE to remove all callbacks for event
+     */
+    function event($event, $value = NULL, $callback = NULL)
+    {
+        static $events;
+
+        // Adding or removing a callback?
+        if($callback !== NULL)
+        {
+            if($callback)
+            {
+                $events[$event][] = $callback;
+            }
+            else
+            {
+                unset($events[$event]);
+            }
+        }
+        elseif(isset($events[$event])) // Fire a callback
+        {
+            foreach($events[$event] as $function)
+            {
+                $value = call_user_func($function, $value);
+            }
+            return $value;
+        }
+    }
+
+
     public function add3dots($string,$repl,$limit)
     {
       if(strlen($string)>$limit)
