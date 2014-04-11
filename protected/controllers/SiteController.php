6ae2a2aa7d33cc1546c37b98b6dd988db5d1fade
reset <reset>
2014-04-11 12:34:39 +0000

    register updated, mobile version added, modile detect module added, left menu updated

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -12,7 +12,7 @@ class SiteController extends Controller
 			// captcha action renders the CAPTCHA image displayed on the contact page
 			'captcha'=>array(
 				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
+				//'backColor'=>0xFFFFFF,
 			),
 			// page action renders "static" pages stored under 'protected/views/site/pages'
 			// They can be accessed via: index.php?r=site/page&view=FileName
