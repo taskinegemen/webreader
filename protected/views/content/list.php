4429e18429abed56cf9fb3c54533e0fc487c829f
egemen <egemen@linden-tech.com>
2014-04-14 08:46:23 +0300

    lazy loading problem solved&performance optimization

--- protected/views/content/list.php
+++ protected/views/content/list.php
@@ -55,21 +55,21 @@ $this->pageTitle=Yii::app()->name;
 			</div>
 		</div><!-- /market_page_container -->
 <script type="text/javascript">
-	 
+    $( document ).ready(function() { 
     function getCurrency(code){
-    	var type;
-    	switch (code){
-    		case '949':
-    		type='TL';
-    		break;
-    		case '998':
-    		type='$';
-    		break;
-    		case '978':
-    		type='€';
-    		break;
-    	}
-    	return type;
+        var type;
+        switch (code){
+            case '949':
+            type='TL';
+            break;
+            case '998':
+            type='$';
+            break;
+            case '978':
+            type='€';
+            break;
+        }
+        return type;
     }
 
     function d2h(d) {
@@ -89,17 +89,17 @@ $this->pageTitle=Yii::app()->name;
         return str%8;
     }
 
-        var kerbela=$(window).kerbelainit();
+       var kerbela=$(window).kerbelainit();
         kerbela.setRequestedHttpService('catalog');
         var ticket=kerbela.getTicket();
         var auth=kerbela.getAuthTicket();
         var HTTP_service_ticket=ticket.HTTP_service_ticket;
-        var organisationId='<?php echo Yii::app()->getBaseUrl(true);?>';
+        var organisationId='http://reader.okutus.com';
         console.log(organisationId);
         var myRe = /.(\w+)\.(com|net|edu|mil|gov)/g;
         var myArray = myRe.exec(organisationId);
         organisationId=myArray[1];
-        var server_organisationId="<?php echo Yii::app()->params['organisation_id']; ?>";
+        var server_organisationId="seviye";
         if(server_organisationId!=''){
             organisationId=server_organisationId;
         }
@@ -110,40 +110,34 @@ $this->pageTitle=Yii::app()->name;
           data: { attributes: '{"organisationId":["'+organisationId+'"]}', auth: auth, http_service_ticket: HTTP_service_ticket, type:"web"}
         })
           .done(function( result ) {
-          	var data = JSON.parse(result);
-        	var author; 
+            var data = JSON.parse(result);
+            var author="Seviye Yayınları"; 
               
+      
             $.each( data.result, function( key, book ) {
-				$.ajax({
-		          type: "POST",
-		          url: "<?php echo Yii::app()->params['catalog_host'];?>/api/getMetaValue",
-		          data: { id: book.contentId, metaKey:'author', auth: auth, http_service_ticket: HTTP_service_ticket, type:"web"}
-		        }).done(function( res ) {
-		        	var authorRes=JSON.parse(res).result;
-		        	if(authorRes){
-		        		author=authorRes;
-		        	}else{
-		        		author = '-';
-		        	}
-		        });
-
-		        var bookthumbnail = "";
-                var image_data = "";
+                /**
                 $.ajax({
-                      url: "<?php echo Yii::app()->params['catalog_host'];?>/api/getThumbnail/id/"+book.contentId
-                    }).done(function(result1) {
-                      console.log(result1);
-                      bookthumbnail = result1;
-                    });
-                if(bookthumbnail){
-                    image_data = "<?php echo Yii::app()->params['catalog_host'];?>/api/getThumbnail/id/"+book.contentId;
-                }
-                else{
+                  type: "POST",
+                  url: "http://bigcat.okutus.com/api/getMetaValue",
+                  data: { id: book.contentId, metaKey:'author', auth: auth, http_service_ticket: HTTP_service_ticket, type:"web"}
+                }).done(function( res ) {
+                    var authorRes=JSON.parse(res).result;
+                    if(authorRes){
+                        author=authorRes;
+                    }else{
+                        author = '-';
+                    }
+                });
+                */
+                //var bookthumbnail = "<?php echo Yii::app()->params['catalog_host'];?>/api/getThumbnail?id=5ZLCqyyAHL6Q3vlukDRZQhH7UbMupEr1sRzFVRXxH0AC";
+                var bookthumbnail = "<?php echo Yii::app()->params['catalog_host'];?>/api/getThumbnail?id="+book.contentId;
+
+                if(!bookthumbnail){
                     var imageid = stringToHex(book.contentId);
-                    image_data = "<?php echo Yii::app()->request->baseUrl; ?>/css/covers/cover"+imageid+".jpg";
+                    bookthumbnail = "/css/covers/cover"+imageid+".jpg";
                 }
 
-				var card='<div class="';
+                var card='<div class="';
         if (book.contentIsForSale=='Free') {
           card+='category_2';
         }else{
@@ -151,29 +145,32 @@ $this->pageTitle=Yii::app()->name;
         };
         card+=' item">\
         <div class="reader_book_card">\
-					<div class="reader_book_card_book_cover">\
-					<a href="<?php echo Yii::app()->request->baseUrl; ?>/content/details/'+book.contentId+'">\
-					<img src="<?php echo Yii::app()->params['catalog_host'];?>/api/getThumbnail?id='+book.contentId+'" style="width:198px; height:264px" /></div></a>\
-					<div class="reader_book_card_info_container">\
-						<div class="reader_market_book_name tip" data-original-title="'+book.contentTitle+'">'+book.contentTitle+'</div>\
-						<button class="reader_book_card_options_button pop-bottom" data-title="Bottom"></button>\
-						<div class="clearfix"></div>\
-						<div class="reader_book_card_writer_name tip" data-original-title="'+author+'">'+author+'</div>\
-						<div class="reader_book_price">';
-				if (book.contentIsForSale=='Free') {
-					card+='Ücretsiz';
-				}else{
-					card+=book.contentPrice+' '+getCurrency(book.contentPriceCurrencyCode);
-				};
-				card+='</div>\
-					</div>\
-				</div></div>';
-				
-				$('#filter-items').append(card);
+                    <div class="reader_book_card_book_cover">\
+                    <a href="/content/details/'+book.contentId+'">\
+                    <img data-src="'+bookthumbnail+'" class="lazyimgs" style="width:198px; height:264px"/></div></a>\
+                    <div class="reader_book_card_info_container">\
+                        <div class="reader_market_book_name tip" data-original-title="'+book.contentTitle+'">'+book.contentTitle+'</div>\
+                        <button class="reader_book_card_options_button pop-bottom" data-title="Bottom"></button>\
+                        <div class="clearfix"></div>\
+                        <div class="reader_book_card_writer_name tip" data-original-title="'+author+'">'+author+'</div>\
+                        <div class="reader_book_price">';
+                if (book.contentIsForSale=='Free') {
+                    card+='Ücretsiz';
+                }else{
+                    card+=book.contentPrice+' '+getCurrency(book.contentPriceCurrencyCode);
+                };
+                card+='</div>\
+                    </div>\
+                </div></div>';
+                
+                $('#filter-items').append(card);
             });
           });
       if( !$('#sidebar').hasClass('mini-menu')) $('#sidebar').addClass('mini-menu');
 if( !$('#main-content').hasClass('margin-left-50')) $('#main-content').addClass('margin-left-50');
+$('img.lazyimgs').lazy();
+
+    });
 </script> 
 
 
