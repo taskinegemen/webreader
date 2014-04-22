e9e3bccfb5f320365360f62719c1bd799473ddac
root <root@mail.lindneo.com>
2014-04-22 13:24:00 +0000

    sddsf

--- protected/config/main.php
+++ protected/config/main.php
@@ -144,7 +144,7 @@ return array(
      		'tr_TR' => 'Türkçe',
      		'en_US' => 'English'
      		),
-		'organisation_id'=>'linden_team',
+		'organisation_id'=>'seviye',
 		// this is used in contact page
 		'adminEmail'=>'pacific@linden-tech.com',
 
